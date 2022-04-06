02562221cd5625e1c38ee5d19514917cf04e3d3f
artelee <ehlee@euclidsoft.co.kr>
2022-04-06 10:41:10 +0900

    [main] 사용하지 않는 변수 정리

--- main.go
+++ main.go
@@ -13,7 +13,6 @@ func main() {
 	// The request responds to a url matching:  /welcome?firstname=Jane&lastname=Doe
 	router.GET("/welcome", func(c *gin.Context) {
 		name := c.DefaultQuery("name", "머글")
-		//lastname := c.Query("lastname") // shortcut for c.Request.URL.Query().Get("lastname")
 
 		c.String(http.StatusOK, api.GetWelcomeMessage(name))
 	})
